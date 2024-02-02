a25bc1cb8f18ee7f4df915d32ca5ac53e5679e76
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-02-01 21:11:59 -0300

    Criação do projeto MVC + Adição arquivo gitignore + Migration de conexão com o banco de dados

---
+++ AgendaMVC/AgendaMVC/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AgendaMVC.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+}
